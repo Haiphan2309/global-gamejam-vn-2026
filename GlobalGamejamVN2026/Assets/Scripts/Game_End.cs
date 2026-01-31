35003a472166c2df1bb0f447d6875e3d5d845305
ThaiBaoak <127424971+Baocheoak@users.noreply.github.com>
2026-01-31 22:20:17 +0700

    Hot fix panel

--- GlobalGamejamVN2026/Assets/Scripts/Game_End.cs
+++ GlobalGamejamVN2026/Assets/Scripts/Game_End.cs
@@ -5,19 +5,29 @@ using TMPro;
 using UnityEngine.SceneManagement;
 
 using UnityEngine.SocialPlatforms.Impl;
+using DG.Tweening;
 
 public class Game_End : MonoBehaviour
 {
     [SerializeField] private TMP_Text resultText;
     [SerializeField] private TMP_Text resultText_shadow;
+    [SerializeField] private GameObject hanabi;
+    [SerializeField] private GameObject button;
 
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text scoreText2;
     [SerializeField] private float timeSet = 5f;
+    [SerializeField] private RectTransform panel;
+    private bool isWin = false;
     
 
     void OnEnable()
     {
+        panel.anchoredPosition = new Vector2(0, -10f);
+        resultText.gameObject.SetActive(false);
+        resultText_shadow.gameObject.SetActive(false);
+        hanabi.SetActive(false);
+        button.SetActive(false);
         if (GamePlayManager.Instance == null)
             return;
 
@@ -29,6 +39,8 @@ public class Game_End : MonoBehaviour
 
     void OnDisable()
     {
+        resultText.gameObject.SetActive(false);
+        resultText_shadow.gameObject.SetActive(false);
         if (GamePlayManager.Instance == null)
             return;
 
@@ -38,6 +50,7 @@ public class Game_End : MonoBehaviour
 
     public void ShowScoreWin(int scoreValue)
     {
+        isWin = true;
         Debug.Log("Show Score Win called with score: " + scoreValue);
         resultText.text = "YOU WIN!";
         resultText_shadow.text = "YOU WIN!";
@@ -52,6 +65,7 @@ public class Game_End : MonoBehaviour
         resultText.text = "TRY AGAIN!";
         resultText_shadow.text = "TRY AGAIN!";
         StopAllCoroutines();
+        panel.DOAnchorPosY(0, 1f).SetEase(Ease.OutBack);
         StartCoroutine(ScoreRoutine(scoreValue));
     }
 
@@ -74,6 +88,25 @@ public class Game_End : MonoBehaviour
         // Hiện số cuối cùng chính xác
         scoreText.text = scoreValue.ToString("D2");
         scoreText2.text = scoreValue.ToString("D2");
+
+        if(isWin)
+        {
+            FaceController.Instance.Happy();
+            hanabi.SetActive(true);
+            button.SetActive(true);
+        }
+        else
+        {
+            FaceController.Instance.Angry();
+            button.SetActive(true);
+            Transform children = button.transform.GetChild(2);
+            children.gameObject.SetActive(false);
+        }
+        yield return new WaitForSeconds(0.5f);
+
+        resultText.gameObject.SetActive(true);
+        resultText_shadow.gameObject.SetActive(true);
+
 
     }
 
